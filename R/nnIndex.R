85ddf1f6e397a8d28303d4c39749b0a2b093f3c1
Zongzheng <chaizz@126.com>
2016-08-12 11:54:47 +0800

    up
    up

--- R/nnIndex.R
+++ R/nnIndex.R
@@ -1,71 +1,131 @@
-nnIndex <-
-  function(X,id=1:(X$n),smark=NULL,N=NULL,R=NULL,
-           rm.id=NULL,add.X=NULL,add.id=paste("add",1:(add.X$n),sep=""),
-           buffer=FALSE,buf.xwid=5,buf.ywid=5,exclusion=FALSE){
-    ppp=rebild.ppp(X=X,id=id,rm.id=rm.id,add.X=add.X,add.id=add.id)
-    buf.ppp<-buffer(X=ppp,buf.xwid=buf.xwid,buf.ywid=buf.ywid)
-    zone=as.data.frame(buf.ppp)[c("id","x","y","zone")]
-    data<-as.data.frame(buf.ppp)
-    loca<-data.frame(X$x,X$y)
-    d<-as.data.frame(as.matrix(dist(loca,"euclidean")))
-    colnames(d)=id
-    data1<-as.data.frame(cbind(id=id,d))
-    data2<-lapply(split(data1[,-c(which(colnames(data1)=="id"))],list(data1$id)),sort)
-    data3<-lapply(data2,function(x) x[-1])
-    if (!is.null(N)){
-      nndist <- applynbd(X=ppp,N=N,function(dists, ...){sort(dists)},exclude=TRUE)
-      data4<-list_to_matrix(lapply(data3,names))[,1:N]
-      nnid<-t(matrix(as.numeric(as.character(data4)),nrow=nrow(data)))
-      if(N==1){
-        nndist<-as.data.frame(nndist)
-        nnid<-as.data.frame(nnid)
-      }else{
-        nndist<-as.data.frame(t(nndist))
-        nnid<-as.data.frame(t(nnid))}
-    }
-    if(!is.null(R)){
-      minnndist=minnndist(X=ppp)
-      if(R<=minnndist(X=ppp))
-        stop(paste("R must exceed the minimum nearest-neighbour distance (",minnndist,")",sep=""))
-      nndist <- applynbd(X=ppp,R=R,function(dists, ...){sort(dists)},exclude=TRUE)
-      nndist<-list_to_matrix(nndist)
-      data3a<-lapply(data3,function(x,n) x[which(x<=n)],n=R)
-      data4a<-list_to_matrix(lapply(data3a,names))
-      nnid<-matrix(as.numeric(as.character(data4)),nrow=nrow(data))
-      if(nrow(nndist)==1){
-        nndist<-as.data.frame(t(nndist))
-        nnid<-as.data.frame(t(nnid))
-      }else{
-        nndist<-as.data.frame(nndist)
-        nnid<-as.data.frame(nnid)}
-    }
-    colnames(nndist)=paste("dist",1:ncol(nndist),sep="")
-    colnames(nnid)=paste("id",1:ncol(nnid),sep="")
-    nnid<-cbind(id=data$id,nnid)
-    C<-list()
-    H<-list()
-    for (i in 1:length(smark)){
-      for(m in 1:ncol(nnid)){
-        C[m][[1]]<-data[smark[i]][,1][match(nnid[,m],data$id)] 
-      }
-      H[[i]]<-sapply(C,as.matrix)
-    }
-    nnIndex<-lapply(H,as.data.frame)
-    names(nnIndex)<-paste("nn",smark,sep="")
-    for (i in 1:length(nnIndex)){
-      names(nnIndex[[i]])=c(smark[i],paste(smark[i],1:(length(names(nnIndex[[i]]))-1),sep=""))
-    }
-    nnIndex$nndist=nndist
-    nnIndex$nnid=nnid
-    if(exclusion){
-      nnIndex<-lapply(nnIndex,function(x) x[,-1])
-    }else{nnIndex<-nnIndex}
-    nnIndex$zone=zone
-    if(buffer){
-      nnIndex<-nnIndex
+nnIndex<-function(X,id=1:(X$n),smark=NULL,N=NULL,R=NULL,
+                  rm.id=NULL,add.X=NULL,add.id=paste("add",1:(add.X$n),sep=""),
+                  buffer=FALSE,buf.xwid=5,buf.ywid=5,exclusion=FALSE){
+  ppp=rebild.ppp(X=X,id=id,rm.id=rm.id,add.X=add.X,add.id=add.id)
+  buf.ppp<-buffer(X=ppp,buf.xwid=buf.xwid,buf.ywid=buf.ywid)
+  zone=as.data.frame(buf.ppp)[c("id","x","y","zone")]
+  data<-as.data.frame(buf.ppp)
+  loca<-data.frame(X$x,X$y)
+  d<-as.data.frame(as.matrix(dist(loca,"euclidean")))
+  colnames(d)=id
+  data1<-as.data.frame(cbind(id=id,d))
+  data2<-lapply(split(data1[,-c(which(colnames(data1)=="id"))],list(data1$id)),sort)
+  data3<-lapply(data2,function(x) x[-1])
+  if (!is.null(N)){
+    nndist <- applynbd(X=ppp,N=N,function(dists, ...){sort(dists)},exclude=TRUE)
+    data4<-list_to_matrix(lapply(data3,names))[,1:N]
+    nnid<-t(matrix(as.numeric(as.character(data4)),nrow=nrow(data)))
+    if(N==1){
+      nndist<-as.data.frame(nndist)
+      nnid<-as.data.frame(nnid)
     }else{
-      nnIndex<-lapply(nnIndex,function(x) as.data.frame(x)[-which(data$zone=="buffer"),])
-    }
-    nnIndex$data<-buf.ppp
-    return(nnIndex)
+      nndist<-as.data.frame(t(nndist))
+      nnid<-as.data.frame(t(nnid))}
   }
+  if(!is.null(R)){
+    minnndist=minnndist(X=ppp)
+    if(R<=minnndist(X=ppp))
+      stop(paste("R must exceed the minimum nearest-neighbour distance (",minnndist,")",sep=""))
+    nndist <- applynbd(X=ppp,R=R,function(dists, ...){sort(dists)},exclude=TRUE)
+    nndist<-list_to_matrix(nndist)
+    data3a<-lapply(data3,function(x,n) x[which(x<=n)],n=R)
+    data4a<-list_to_matrix(lapply(data3a,names))
+    nnid<-matrix(as.numeric(as.character(data4)),nrow=nrow(data))
+    if(nrow(nndist)==1){
+      nndist<-as.data.frame(t(nndist))
+      nnid<-as.data.frame(t(nnid))
+    }else{
+      nndist<-as.data.frame(nndist)
+      nnid<-as.data.frame(nnid)}
+  }
+  colnames(nndist)=paste("dist",1:ncol(nndist),sep="")
+  colnames(nnid)=paste("id",1:ncol(nnid),sep="")
+  nnid<-cbind(id=data$id,nnid)
+  
+  C<-list()
+  H<-list()
+  for (i in 1:length(smark)){
+    for(m in 1:ncol(nnid)){
+      C[m][[1]]<-data[smark[i]][,1][match(nnid[,m],data$id)] 
+    }
+    H[[i]]<-sapply(C,as.matrix)
+  }
+  nnIndex<-lapply(H,as.data.frame)
+  names(nnIndex)<-paste("nn",smark,sep="")
+  for (i in 1:length(nnIndex)){
+    names(nnIndex[[i]])=c(smark[i],paste(smark[i],1:(length(names(nnIndex[[i]]))-1),sep=""))
+  }
+  nnIndex$nndist=nndist
+  nnIndex$nnid=nnid
+  if(exclusion){
+    nnIndex<-lapply(nnIndex,function(x) x[,-1])
+  }else{nnIndex<-nnIndex}
+  nnIndex$zone=zone
+  if(buffer){
+    nnIndex<-nnIndex
+  }else{
+    nnIndex<-lapply(nnIndex,function(x) as.data.frame(x)[-which(data$zone=="buffer"),])
+  }
+  nnIndex$data<-buf.ppp
+  return(nnIndex)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/rebild.ppp.R
+++ R/rebild.ppp.R
@@ -1,31 +1,24 @@
-rebild.ppp<-function(X,id=1:(X$n),rm.id=NULL,add.id=NULL,add.x=NULL,add.y=NULL,add.mark=NULL){
-  data<-as.data.frame(X)
-  data$id<-id
-  if (!is.null(rm.id)){
-    cat("Warning:rm.id should be included in the id")
-    newX<-data[-match(rm.id,id),]
-    newX.ppp<-ppp(x=newX$x,y=newX$y,X$window$xrange,X$window$yrange,
-                  marks=newX[,-c(1:2)])
+rebild.ppp<-function(X,id=1:(X$n),rm.id=NULL,add.X=NULL,add.id=paste("add",1:(add.X$n),sep="")){
+  if(is.null(X$marks)){
+    marks(X)=id
+  }else{
+    marks(X)=data.frame(id=id,X$marks)
   }
-  else if (!is.null(add.id)){
+  newX=X
+  if (!is.null(rm.id)){
+    if(any(is.na(match(rm.id,id))))
+      stop("rm.id should be included in the id")
+    newX<-X[-match(rm.id,id),]
+  }
+  if (!is.null(add.X)){
     if(any(intersect(add.id,id)))
       stop("add.id must different with id")
-    if(!is.null(add.mark)){
-      cat("Warning:please ensure same variables in add.mark must with same colname of X$mark")
-      if (!all(length(add.id)==length(add.x),length(add.id)==length(add.y),
-               length(add.id)==nrow(add.mark)))
-        stop("add.id,add.x,add.y,and nrow of add.mark must with same length")
-      add.data<-as.data.frame(cbind(x=add.x,y=add.y,add.mark))
+    if(is.null(add.X$marks)){
+      marks(add.X)=add.id
     }else{
-      if (!all(length(add.id)==length(add.x),length(add.id)==length(add.y)))
-        stop("add.id,add.x,and add.y must with same length")
-      add.data<-as.data.frame(cbind(x=add.x,y=add.y))
+      marks(add.X)=data.frame(id=add.id,add.X$marks)
     }
-    add.data$id<-add.id
-    newX=merge(add.data,data,all=TRUE)
-    newX.ppp<-ppp(x=newX$x,y=newX$y,X$window$xrange,X$window$yrange,
-                  marks=newX[,-c(1:2)])
-  }else{newX.ppp=ppp(x=data$x,y=data$y,X$window$xrange,X$window$yrange,
-                     marks=data[,-c(1:2)])}
-  return(newX.ppp)
+    newX<-superimpose(X,add.X)
+  } 
+  return(newX)
 }
