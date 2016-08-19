611a3408a7194df4d07754d42593f2e6a195d341
Zongzheng <chaizz@126.com>
2016-08-19 09:02:07 +0800

    R package
    R package

--- R/fsasN4.R
+++ R/fsasN4.R
@@ -1,15 +1,16 @@
 fsasN4 <-
 function(nnattri,match.fun,para=NULL){
   if(is.null(para)){
-    I<-apply(nnattri,1,match.fun)
+    Index<-apply(nnattri,1,match.fun)
   }else{
-    I<-apply(nnattri,1,match.fun,para=para)
+    Index<-apply(nnattri,1,match.fun,para=para)
   }
+    Index<-I
+    Index<-as.numeric(as.character(Index))
     I<-factor(I,levels=c(0,0.25,0.5,0.75,1))
     Icount<-table(I)
     Ifreq<- Icount/sum(Icount)
     meanI<-sum(t(Icount)*as.numeric(names(Icount)))/sum(Icount)
-    sdI<-sd(as.numeric(as.character(I)))
-    list(result=cbind(nnattri,I),Icount=Icount,
-         Ifreq=Ifreq,meanI=meanI,sdI=sdI)
+    list(result=cbind(nnattri,Index),Icount=Icount,
+         Ifreq=Ifreq,meanI=meanI)
   }
---
+++ R/fsh.R
@@ -0,0 +1,7 @@
+fsh <-
+function(index,optm){
+  rms<-function(actual,predicted){
+    return(sqrt(mean((actual-predicted)^2)))
+  }
+  return(1-rms(index,optm))
+}
