464d246cb9bdd84a843c112908907f6d9ff40edc
Zongzheng <chaizz@126.com>
2016-08-19 09:30:19 +0800

    R package
    R package

--- R/fsasN4.R
+++ R/fsasN4.R
@@ -1,16 +1,15 @@
 fsasN4 <-
 function(nnattri,match.fun,para=NULL){
   if(is.null(para)){
-    Index<-apply(nnattri,1,match.fun)
+    ind<-apply(nnattri,1,match.fun)
   }else{
-    Index<-apply(nnattri,1,match.fun,para=para)
+    ind<-apply(nnattri,1,match.fun,para=para)
   }
-    Index<-I
-    Index<-as.numeric(as.character(Index))
-    I<-factor(I,levels=c(0,0.25,0.5,0.75,1))
-    Icount<-table(I)
+    Ind<-ind
+    Ind<-factor(Ind,levels=c(0,0.25,0.5,0.75,1))
+    Icount<-table(Ind)
     Ifreq<- Icount/sum(Icount)
     meanI<-sum(t(Icount)*as.numeric(names(Icount)))/sum(Icount)
-    list(result=cbind(nnattri,Index),Icount=Icount,
+    list(result=cbind(nnattri,index=ind),Icount=Icount,
          Ifreq=Ifreq,meanI=meanI)
   }
